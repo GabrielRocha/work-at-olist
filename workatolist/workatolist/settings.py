900648da072ec942883687a914c522585a7b8341
Gabriel Rocha <gabrielrocha@catho.com.br>
2018-01-01 10:49:58 -0200

    Default values for environment variables

--- workatolist/workatolist/settings.py
+++ workatolist/workatolist/settings.py
@@ -10,12 +10,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = config("SECRET_KEY", default="MY SECRET KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG")
+DEBUG = config("DEBUG", default=False, cast=config.boolean)
 
-ALLOWED_HOSTS = [config("ALLOWED_HOSTS")]
+ALLOWED_HOSTS = [config("ALLOWED_HOSTS", default=['*'])]
 
 
 # Application definition
