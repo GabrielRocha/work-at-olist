5b5be66ae2609f720cff18bfe845ecbbbed28f1b
Gabriel Rocha <gabrielrocha@catho.com.br>
2018-01-01 15:01:05 -0200

    Channel relations with categories

--- workatolist/channels/tests/models/test_channel.py
+++ workatolist/channels/tests/models/test_channel.py
@@ -1,5 +1,5 @@
 import pytest
-from channels.models import Channel
+from channels.models import Channel, Category
 from django.db import models
 
 
@@ -20,3 +20,25 @@ def test_create_channel():
 @pytest.mark.django_db
 def test_slug_channel(channel):
     assert channel.slug == "walmart"
+
+
+@pytest.mark.django_db
+def test_channels_categories(channel, category):
+    assert channel.categories.count() == 1
+    assert category in channel.categories.iterator()
+
+
+@pytest.mark.django_db
+def test_channels_two_levels_categories(channel, category):
+    second_level_category = Category.objects.create(channel=channel,
+                                                    name="Comedy", parent=category)
+    assert second_level_category in channel.categories.first().get_descendants()
+
+
+@pytest.mark.django_db
+def test_channels_three_levels_categories(channel, category):
+    second_level_category = Category.objects.create(channel=channel,
+                                                    name="Comedy", parent=category)
+    third_level_category = Category.objects.create(channel=channel,
+                                                    name="Studio", parent=second_level_category)
+    assert third_level_category in channel.categories.first().get_descendants()
