59d3881dc237377593d2b912182f0556f9b2112e
Gabriel Rocha <gabrielrocha@catho.com.br>
2018-01-02 16:17:06 -0200

    Populate the database with multiprocessing and query quantity optimized

--- workatolist/channels/management/commands/importcategories.py
+++ workatolist/channels/management/commands/importcategories.py
@@ -1,12 +1,34 @@
+from itertools import groupby
 from django.core.management.base import BaseCommand
+from channels.models import Channel, Category
+import pathos.multiprocessing as mp
+from django.db import connection
 
 
 class Command(BaseCommand):
-    help = "My shiny new management command."
 
     def add_arguments(self, parser):
         parser.add_argument('channel', type=str, help="Channel name")
         parser.add_argument('categories_file', type=str, help="Categories file path")
 
     def handle(self, *args, **options):
-        raise NotImplementedError()
+        channel, _ = Channel.objects.get_or_create(name=options['channel'])
+        channel.categories.all().delete()
+        connection.close()
+        with open(options['categories_file']) as categories:
+            raws = categories.readlines()
+            args = ((category.strip(), list(groups), channel)
+                    for category, groups in groupby(raws, lambda x: x.split("/")[0].strip()))
+        with mp.Pool() as pool:
+            pool.starmap(save_categories, args)
+
+
+def save_categories(category, groups, channel):
+    first_level_category = Category.objects.create(channel=channel, name=category, parent=None)
+    category_objects = {category: first_level_category}
+    for group in list(groups)[1:]:
+        categories = group.split("/")
+        category = categories[-1].strip()
+        parent = category_objects["/".join(categories[:-1]).strip()]
+        category_objects["/".join(categories).strip()] = Category.objects.create(
+            channel=channel, name=category, parent=parent)
--- workatolist/channels/tests/commands/test_importcategories.py
+++ workatolist/channels/tests/commands/test_importcategories.py
@@ -1,4 +1,5 @@
 from channels.management.commands import importcategories
+from channels.models import Channel, Category
 import argparse
 import pytest
 
@@ -14,3 +15,28 @@ def test_add_arguments(command):
     args = parser.parse_args(["Walmart", "path"])
     assert args.channel == "Walmart"
     assert args.categories_file == "path"
+
+
+@pytest.mark.django_db
+@pytest.mark.django_db(transaction=True)
+def test_handle_command(command):
+    command.handle(categories_file='channels/tests/fixtures/categories.txt',
+                   channel="Walmart")
+    book_slug = "walmart-books-national-literature-science-fiction"
+    computers_slug = "walmart-computers-tablets"
+    assert Channel.objects.filter(name='Walmart').count() == 1
+    assert Category.objects.count() == 23
+    assert Category.objects.filter(slug=computers_slug).exists()
+    assert Category.objects.filter(slug=book_slug).exists()
+
+
+@pytest.mark.django_db
+def test_importer_save_categories(channel):
+    args = ['Computers',
+              ['Computers\n', 'Computers / Notebooks\n'],
+              channel]
+    importcategories.save_categories(*args)
+    assert Category.objects.filter(name="Computers").exists()
+    parent = Category.objects.get(name="Computers")
+    assert Category.objects.filter(name="Notebooks",
+                                   parent=parent).exists()
